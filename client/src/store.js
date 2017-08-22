0164e1765ee4a033a149dfca44dbcc7d14ac30df
PROB8 <blackb8r@gmail.com>
2017-08-21 22:00:50 -0400

    remove remote-redux-devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
 import zodiacReducer from './reducers';
 import thunk from 'redux-thunk';
-import devToolsEnhancer from 'remote-redux-devtools';
 
 export default createStore(zodiacReducer, window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION(), applyMiddleware(thunk));
